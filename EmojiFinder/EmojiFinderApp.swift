e6206df109a363ba456960acb35e71346cd73840
adityahimaone <adityahimaone@gmail.com>
2024-01-25 19:47:03 +0700

    Initial Commit

---
+++ EmojiFinder/EmojiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiFinderApp.swift
+//  EmojiFinder
+//
+//  Created by Aditya Himawan on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
